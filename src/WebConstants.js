3e59844a168d9612c3d14b414c1f2a9949255be8
Jay Hodgson <jay.hodgson@sagebase.org>
2018-01-22 11:47:14 -0800

    fix repo endpoint so that it works week to week

--- src/WebConstants.js
+++ src/WebConstants.js
@@ -1,3 +1,3 @@
 export const SYNAPSE_ORG = 'https://www.synapse.org/';
 export const PROFILE_PAGE = '#!Profile:';
-export const REPO_ENDPOINT = 'https://repo-prod-211-0.prod.sagebase.org/repo/v1/';
+export const REPO_ENDPOINT = 'https://repo-prod.prod.sagebase.org/repo/v1/';
